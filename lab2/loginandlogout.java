81821c5c8755b4abfe6d59b8cef384d3fdc2cb31
VenkataLakshmipathi <161968736+VenkataLakshmipathi@users.noreply.github.com>
2024-05-17 13:25:19 +0530

    Update loginandlogout.java

--- lab2/loginandlogout.java
+++ lab2/loginandlogout.java
@@ -1,6 +1,6 @@
 package javafullstack;
 import java.util.Scanner;
- public class Loginandlogout {
+ public class loginandlogout {
   public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
    String username = "Venkat";
